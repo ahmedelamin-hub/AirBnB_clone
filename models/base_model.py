4569913230d15850eec983759b1d759642c28830
root <ahmedelamin42@gmail.com>
2024-03-09 12:23:45 -0800

    g

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-import models
 import uuid
 from datetime import datetime
+import models
+
 
 class BaseModel:
-    """Represents the base model."""
-    
+    """Represents a base model with common attributes/methods for other classes."""
     def __init__(self, *args, **kwargs):
-        """Initializes a new BaseModel instance."""
+        """Initialization of the base model."""
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key in ['created_at', 'updated_at']:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
@@ -21,17 +21,17 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """Returns the string representation of the BaseModel instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """String representation of the BaseModel class."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the instance's updated_at attribute."""
+        """Updates 'updated_at' with the current datetime."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of the instance."""
-        dict_copy = dict(self.__dict__)
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
         dict_copy['created_at'] = self.created_at.isoformat()
         dict_copy['updated_at'] = self.updated_at.isoformat()
