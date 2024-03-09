e5bbc56f327790c6e5b12f4039707b223cdb37ed
root <ahmedelamin42@gmail.com>
2024-03-09 12:51:02 -0800

    g

--- models/base_model.py
+++ models/base_model.py
@@ -4,22 +4,29 @@ Module: base_model
 Defines the BaseModel class.
 """
 
+
 import uuid
 from datetime import datetime
-
 
 class BaseModel:
     """
     Defines the BaseModel class.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -43,4 +50,4 @@ class BaseModel:
         obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        return obj_dicti
