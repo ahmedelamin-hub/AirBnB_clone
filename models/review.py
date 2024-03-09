4c3d642d124798d94031493b5893c71f5dd19a5f
root <ahmedelamin42@gmail.com>
2024-03-09 11:45:43 -0800

    g

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """Defines the Review class."""
-
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-    """Represents a review for a HBNB project."""
-    place_id = ""  # Will be the Place.id
-    user_id = ""  # Will be the User.id
+    """Represent a review.
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
+    place_id = ""
+    user_id = ""
     text = ""
