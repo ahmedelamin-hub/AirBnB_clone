e7ccb75bfc8497e96c6dc36b20d15608490277ab
root <ahmedelamin42@gmail.com>
2024-03-09 04:12:20 -0800

    H

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""a class BaseModel that defines attributes/methods for objects"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+
+    def __init__(self):
+        """Initializes the BaseModel instance."""
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns the string representation of the Base."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
