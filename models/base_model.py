0722e9051fb1b3031bd3c4272b1fb32a908f6bb3
root <ahmedelamin42@gmail.com>
2024-03-10 06:02:06 -0700

    g

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
-"""class where all child classes inherit"""
+"""The class base for all other instances"""
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """the user class"""
+    """Defines all common attributes/methods for other classes."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new instance of BaseModel.
+        
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key-value pairs of attributes.
+        """
         if kwargs:
-            for k, v in kwargs.items():
-                if k != "__class__":
-                    if k == "created_at" or k == "updated_at":
-                        v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, k, v)
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns the string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Updates the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        new_dict = dict(self.__dict__)
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines the City class."""
-
-from models.base_model import BaseModel
-
-class City(BaseModel):
-    """Represents a city for a HBNB project."""
-    state_id = ""
-    name = ""
