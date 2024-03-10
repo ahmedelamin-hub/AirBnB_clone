92df54ffe63cdc7ce4bf0bd4ce7b3327be6a5e1c
root <ahmedelamin42@gmail.com>
2024-03-10 06:34:02 -0700

    g

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """
-Defines a BaseModel class.
-This class will be the base for all other classes in our project.
-It includes common attributes and methods that can be inherited.
+Defines the BaseModel class.
+This class is the base for all other classes in the project.
+It handles initialization, serialization, and deserialization of instances.
 """
 
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """
-    Defines the BaseModel class.
+    A base class for other classes to inherit from.
     Attributes:
-        id (str): unique id for each instance.
-        created_at (datetime): the current datetime when an instance is created.
-        updated_at (datetime): the current datetime when an instance is created and
-                               it will be updated every time the instance is changed.
+        id (str): Unique id for each instance.
+        created_at (datetime): The current datetime when an instance is created.
+        updated_at (datetime): The current datetime when an instance is created and
+                               will be updated every time the instance changes.
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initializes a new instance of the BaseModel class.
-        If kwargs is not empty, it initializes the instance attributes accordingly,
-        converting datetime strings back to datetime objects.
-        Otherwise, it creates a new instance with unique id and current datetime.
+        Initializes a new instance of BaseModel.
+        If kwargs is not empty, initializes the instance attributes accordingly.
+        Otherwise, creates a new instance with unique id and current datetime.
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -36,6 +36,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -45,15 +46,16 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the updated_at attribute with the current datetime.
+        Updates the updated_at attribute with the current datetime and saves the
+        instance to the JSON file.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
         Returns a dictionary containing all keys/values of __dict__ of the instance.
         This dictionary also contains the class name under the key '__class__'.
-        The datetime attributes are converted to strings in ISO format.
         """
         dict_rep = self.__dict__.copy()
         dict_rep['__class__'] = self.__class__.__name__
